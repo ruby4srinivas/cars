6c7bb933e24ba12058bde7d62fc08a23b1574344
srinivas <srinivas4ruby@gmail.com>
2015-12-31 12:57:47 +0530

    Revert "First commit"
    This reverts commit 98aec37e8c58698448b15a8ef0a4eac156af4547.

--- app/views/cardetails/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @cardetail, :id, :image, :make_year, :make_car, :model, :price, :price, :created_at, :updated_at
--- test/models/cardetail_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CardetailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
